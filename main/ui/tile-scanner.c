64cbe0f2d88b4745add0060ed9c70934e6f114fd
Nels885 <voirin.lionel@gmail.com>
2021-04-18 11:50:04 +0200

    Added a Scanner ON / OFF btn in the scanner tile for different tests

--- main/ui/tile-scanner.c
+++ main/ui/tile-scanner.c
@@ -1,10 +1,12 @@
 #include "tile-scanner.h"
 
-tile_t scanner_tile;
-
 #define TAG "[tile::wifi::scanner]"
 
-wifiscan_t g_wifiscan;
+static wifiscan_t g_wifiscan;
+static tile_t scanner_tile;
+static widget_button_t scanner_btn;
+
+volatile bool scanner_enabled = false;
 
 void scanner_event_handler(wifiscan_event_t event)
 {
@@ -22,14 +24,44 @@ void scanner_event_handler(wifiscan_event_t event)
   }
 }
 
+void scanner_toggle(struct widget_t *p_widget)
+{
+  if (!scanner_enabled)
+  {
+    /* Enable scanner ! */
+    wifi_set_mode(WIFI_SCANNER);
+
+    /* Change button text. */
+    widget_button_set_text(&scanner_btn, "Scan Off");
+
+    /* Scanner is on. */
+    scanner_enabled = true;
+  }
+  else
+  {
+    /* Stop scanner. */
+    wifi_set_mode(WIFI_OFF);
+
+    /* Set button text. */
+    widget_button_set_text(&scanner_btn, "Scan On");
+
+    /* Scanner is off. */
+    scanner_enabled = false;
+  }
+}
+
 tile_t *tile_scanner_init(void)
 {
   /* Initialize our tile. */
   tile_init(&scanner_tile, NULL);
 
   /* Add a wifiscan widget. */
-  wifiscan_init(&g_wifiscan, &scanner_tile, 5, 5, 230, 230);
+  wifiscan_init(&g_wifiscan, &scanner_tile, 5, 5, 230, 195);
   wifiscan_set_event_handler(&g_wifiscan, scanner_event_handler);
+
+  /* Add scanner button. */
+  widget_button_init(&scanner_btn, &scanner_tile, (240-120)/2, 205, 120, 30, "Scan On");
+  widget_button_set_handler(&scanner_btn, scanner_toggle);
 
   /* Return our tile. */
   return &scanner_tile;
